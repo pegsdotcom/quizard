db1a90bbb198e82fe72a41fdee678b2c161abecd
Pegah Ghadrdan <112897723+pegahdotcom@users.noreply.github.com>
2024-12-01 17:26:02 +0100

    Added questions

---
+++ js/questions.js
@@ -0,0 +1,110 @@
+const questions = [
+    {
+      question: "Vilka av följande jultraditioner är faktiskt vanliga i olika delar av världen? (Välj alla som är korrekta)",
+      type: "multiple",
+      options: [
+        "Att jultomten går på husets tak och slår på dörren för att dela ut presenter",
+        "Att äta 12 olika rätter på julafton",
+        "Att skjuta fyrverkerier vid midnatt på julafton",
+        "Att gömma en julgurka i julgranen och den som hittar den får en present"
+      ],
+      correctAnswers: [1, 3]
+    },
+    {
+      question: "Vilken maträtt äts traditionellt på jul i Japan?",
+      type: "single",
+      options: [
+        "Sushi",
+        "Tempura",
+        "KFC",
+        "Ramen"
+      ],
+      correctAnswers: [2]
+    },
+    {
+      question: "Vilka av följande karaktärer har varit med i populära julfilmer? (Välj alla som stämmer)",
+      type: "multiple",
+      options: [
+        "Jack Skellington",
+        "Elf, en jultomteassistent",
+        "The Grinch",
+        "Frodo Baggins"
+      ],
+      correctAnswers: [0, 1, 2]
+    },
+    {
+      question: "Vilken julhit, som är känd för att vara världens mest sålda julsång genom tiderna, blev först släppt 1973 av Bing Crosby?",
+      type: "single",
+      options: [
+        "Silent Night",
+        "Do They Know It's Christmas",
+        "White Christmas",
+        "Jingle Bells"
+      ],
+      correctAnswers: [2]
+    },
+    {
+      question: "I den populära julsagan 'The Polar Express', vad är det hemliga ordet som används för att bevisa att man verkligen tror på jultomten?",
+      type: "single",
+      options: [
+        "Believe",
+        "Magic",
+        "Santa",
+        "Wish"
+      ],
+      correctAnswers: [0]
+    },
+    {
+      question: "I vilken del av världen betraktas jultomten ibland som en gigantisk, godhjärtad trollkarl snarare än en man med röd kostym?",
+      type: "single",
+      options: [
+        "Skottland",
+        "Island",
+        "Kanada",
+        "Finland"
+      ],
+      correctAnswers: [1]
+    },
+    {
+      question: "Vilken känd julsymbol, som ofta används på julkort, kommer ursprungligen från det medeltida Europa och symboliserade kristen tro?",
+      type: "single",
+      options: [
+        "Julstjärna",
+        "Mistel",
+        "Julgran",
+        "Adventsljusstake"
+      ],
+      correctAnswers: [0]
+    },
+    {
+      question: "I Charles Dickens 'A Christmas Carol', vad heter den första ande som besöker Ebenezer Scrooge?",
+      type: "single",
+      options: [
+        "Ghost of Christmas Past",
+        "Jacob Marley",
+        "Ghost of Christmas Future",
+        "Ghost of Christmas Present"
+      ],
+      correctAnswers: [1]
+    },
+    {
+      question: "Enligt en gammal jultradition i England, vad händer om du kysser någon under misteln på julafton?",
+      type: "single",
+      options: [
+        "Du får tur hela året",
+        "Du får en julklapp",
+        "Du blir förlovad",
+        "Du får ett lyckligt äktenskap"
+      ],
+      correctAnswers: [0]
+    },
+    {
+      question: "Jultomten var ursprungligen baserad på en holländsk legend om en helgonpräst vid namn Sinterklaas, som åkte runt på en åsna och gav presenter till snälla barn.",
+      type: "boolean",
+      options: [
+        "Sant",
+        "Falskt"
+      ],
+      correctAnswers: [0]
+    }
+];
