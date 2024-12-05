530b6108b362662d122badd3847312b387e8f74c
Pegah Ghadrdan <112897723+pegahdotcom@users.noreply.github.com>
2024-12-05 12:16:46 +0100

    Added darkmode

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+const themeToggleBtn = document.getElementById('themeToggle');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode'); 
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');  
+  } else {
+    localStorage.setItem('theme', 'light');  
+  }
+});
+
+
+
 
 
 
